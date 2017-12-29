39e5c0d43a3c45c0ebf5d5965573da2481ccee07
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-12-29 10:15:18 +0000

    nav links and search bar added into browser

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            <li>New In</li>
+            <li>Womens</li>
+            <li>Get Inspired</li>
+          </ul>
+          <form>
+            <input type="search"></input>
+            <button>Search</button>
+          </form>
         </header>
       </div>
     );
